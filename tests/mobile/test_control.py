a9027892eb43d5a6034fbabaa23bf2b3b4d90cb1
Y3ll0w <k.varbarkin@gmail.com>
2023-12-24 23:44:44 +0300

    Added test_control

---
+++ tests/mobile/test_control.py
@@ -0,0 +1,18 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_control_buttons_should_be_shown():
+    # THEN
+    with step('Button "Иой ivi" are shown'):
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Мой ivi"]')).should(have.text('Мой ivi'))
+
+    with step('Button "Каталог" are shown'):
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Каталог"]')).should(have.text('Каталог'))
+
+    with step('Button "Загрузки" are shown'):
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Загрузки"]')).should(have.text('Загрузки'))
+
+    with step('Button "Профиль" are shown'):
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Профиль"]')).should(have.text('Профиль'))
