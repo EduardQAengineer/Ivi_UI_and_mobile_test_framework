824b9c378e39bfc2bdcb74d3699454bc418722f0
Y3ll0w <k.varbarkin@gmail.com>
2023-12-25 03:39:00 +0300

    Added test_select_movie_in_recommends and fix test_serials_sections_should_be_shown

--- tests/mobile/test_my_ivi.py
+++ tests/mobile/test_my_ivi.py
@@ -3,15 +3,19 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
-def test_movie_sections_should_be_shown():
+def test_serials_sections_should_be_shown():
     # WHEN
     with step('Tap on "Мой ivi"'):
         browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Мой ivi"]')).click()
 
     # THEN
-    with step('Check movie sections'):
-        with step('Best shows on subscription - are shown'):
-            browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Лучшие сериалы в подписке"]')).should(have.text('Лучшие сериалы в подписке'))
+    with step('Serials - are shown'):
+        browser.element((AppiumBy.XPATH, '//*[contains(@text, "Сериалы")]')).should(have.text('Сериалы'))
 
-        with step('High-rated subscription serials - are shown'):
-            browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Сериалы с высоким рейтингом по подписке"]')).should(have.text('Сериалы с высоким рейтингом по подписке'))
+
+def test_select_movie_in_recommends():
+    with step('Tap on recommend movie'):
+        browser.element((AppiumBy.XPATH, '(//android.widget.FrameLayout[@resource-id="ru.ivi.client:id/poster"])[2]')).click()
+
+    with step('Button "Смотреть" is shown'):
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Смотреть"]')).should(have.text('Смотреть'))
