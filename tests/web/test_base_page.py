b5242024ca8b92021dbb1a1e4c9b5c55a413f5a8
Y3ll0w <k.varbarkin@gmail.com>
2023-12-28 01:34:52 +0300

    Added test_open_promo

--- tests/web/test_base_page.py
+++ tests/web/test_base_page.py
@@ -3,20 +3,17 @@ from selene import browser, have, be
 
 def test_open_base_page():
     browser.open('/')
-    # browser.element('.nbl-controlButton__caption').should(be.visible).click()
     browser.element('#headerTop').should(have.text('Мой Иви'))
 
 
-def test_search():
+def test_open_movie():
     browser.open('/')
-    # browser.element('.nbl-controlButton__caption').should(be.visible).click()
-    browser.element('[data-test="header_search"]').should(be.visible).click()
-    browser.element('input[data-test="search_input"]').type('Хроники риддика').press_enter()
-    browser.element('.searchBlock.searchBlock_result').should(have.text('Также смотрят'))
-
-
-def test_open_move():
-    browser.open('/')
-    # browser.element('.nbl-controlButton__caption').should(be.visible).click()
     browser.element('[data-content-id="16974"]').should(be.visible).click()
     browser.element('[data-testid="watchTitle"]').should(have.text('Сериал Блеск смотреть онлайн'))
+
+
+def test_open_promo():
+    browser.open('/')
+    browser.element('.teaserPlate.home__teaserPlate').should(be.visible).click()
+    browser.element('.segmentedLanding__title.segmentedLanding__title_default').should(have.text('Подписка Иви'))
+    browser.element('.nbl-button.nbl-button_textAlign_center.nbl-button_style_kioshi.nbl-button_hasBadge_0.segmentedLanding__nbl-button .nbl-button__primaryText').should(have.text('Попробовать 60 дней бесплатно'))
