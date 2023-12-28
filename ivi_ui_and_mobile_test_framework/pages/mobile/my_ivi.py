6c2926c207a896e0d1854af34e2d6bc5ac27cbae
Y3ll0w <k.varbarkin@gmail.com>
2023-12-28 23:21:32 +0300

    Fixed recommend movie locator

--- ivi_ui_and_mobile_test_framework/pages/mobile/my_ivi.py
+++ ivi_ui_and_mobile_test_framework/pages/mobile/my_ivi.py
@@ -15,7 +15,7 @@ def serials_should_be_shown():
 
 def tap_on_recommend_movie():
     with step('Tap on recommend movie'):
-        browser.element((AppiumBy.XPATH, '(//android.widget.FrameLayout[@resource-id="ru.ivi.client:id/poster"])[2]')).click()
+        browser.element((AppiumBy.XPATH, '(//android.widget.FrameLayout[@resource-id="ru.ivi.client:id/poster"])')).click()
 
 
 def button_watch_should_be_shown():
