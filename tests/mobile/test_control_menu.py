14f89172f77d64b4be449c7d2fd31c9ee1aa3e4d
Y3ll0w <k.varbarkin@gmail.com>
2023-12-28 18:40:49 +0300

    Fixed "" > ''

--- tests/mobile/test_control_menu.py
+++ tests/mobile/test_control_menu.py
@@ -3,13 +3,13 @@ import allure
 from qa_guru_python_diploma.pages.mobile import control_menu
 
 
-@allure.title("Control buttons should be shown")
-@allure.epic("Bottom menu")
-@allure.story("Bottom menu")
-@allure.feature("Control buttons in bottom menu")
-@allure.label("microservice", "Bottom menu")
-@allure.label("owner", "allure8")
-@allure.tag("smoke", "regress", "mobile", "critical")
+@allure.title('Control buttons should be shown')
+@allure.epic('Bottom menu')
+@allure.story('Bottom menu')
+@allure.feature('Control buttons in bottom menu')
+@allure.label('microservice', 'Bottom menu')
+@allure.label('owner', 'allure8')
+@allure.tag('smoke', 'regress', 'mobile', 'critical')
 @allure.severity('critical')
 def test_control_buttons_should_be_shown():
     # THEN
--- tests/mobile/test_my_ivi.py
+++ tests/mobile/test_my_ivi.py
@@ -4,13 +4,13 @@ from qa_guru_python_diploma.pages.mobile import my_ivi
 
 
 @allure.epic('My Ivi')
-@allure.story("Movies")
+@allure.story('Movies')
 class TestMyIvi:
-    @allure.title("Serials sections should be shown")
-    @allure.feature("Serials sections")
-    @allure.label("microservice", "My Ivi")
-    @allure.label("owner", "allure8")
-    @allure.tag("regress", "mobile")
+    @allure.title('Serials sections should be shown')
+    @allure.feature('Serials sections')
+    @allure.label('microservice', 'My Ivi')
+    @allure.label('owner', 'allure8')
+    @allure.tag('regress', 'mobile')
     @allure.severity('normal')
     def test_serials_sections_should_be_shown(self):
         # WHEN
@@ -19,11 +19,11 @@ class TestMyIvi:
         # THEN
         my_ivi.serials_should_be_shown()
 
-    @allure.title("Select movie in recommends")
-    @allure.feature("Recommends section")
-    @allure.label("microservice", "My Ivi")
-    @allure.label("owner", "allure8")
-    @allure.tag("regress", "mobile")
+    @allure.title('Select movie in recommends')
+    @allure.feature('Recommends section')
+    @allure.label('microservice', 'My Ivi')
+    @allure.label('owner', 'allure8')
+    @allure.tag('regress', 'mobile')
     @allure.severity('normal')
     def test_select_movie_in_recommends(self):
         # WHEN
