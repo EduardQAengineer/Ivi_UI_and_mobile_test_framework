0ede7012084655dcf7aea7b6a94a5b9c4f0c60aa
Y3ll0w <k.varbarkin@gmail.com>
2023-12-28 18:26:25 +0300

    Fixed marks on mobile tests

--- tests/mobile/test_my_ivi.py
+++ tests/mobile/test_my_ivi.py
@@ -1,37 +1,34 @@
 import allure
-import pytest
 
 from qa_guru_python_diploma.pages.mobile import my_ivi
-from tests.marks import microservice, layer, owner
 
 
-@allure.title("Serials sections should be shown")
-@allure.story("Serials")
-@allure.feature("Serials sections")
-@microservice("Serials")
-@layer("Mobile")
-@owner("allure8")
-@pytest.mark.regress
-@pytest.mark.mobile
-def test_serials_sections_should_be_shown():
-    # WHEN
-    my_ivi.tap_on_my_ivi()
+@allure.epic('My Ivi')
+@allure.story("Movies")
+class TestMyIvi:
+    @allure.title("Serials sections should be shown")
+    @allure.feature("Serials sections")
+    @allure.label("microservice", "My Ivi")
+    @allure.label("owner", "allure8")
+    @allure.tag("regress", "mobile")
+    @allure.severity('normal')
+    def test_serials_sections_should_be_shown(self):
+        # WHEN
+        my_ivi.tap_on_my_ivi()
 
-    # THEN
-    my_ivi.serials_should_be_shown()
+        # THEN
+        my_ivi.serials_should_be_shown()
 
 
-@allure.title("Select movie in recommends")
-@allure.story("Recommended movies")
-@allure.feature("Recommends section")
-@microservice("Recommends")
-@layer("Mobile")
-@owner("allure8")
-@pytest.mark.regress
-@pytest.mark.mobile
-def test_select_movie_in_recommends():
-    # WHEN
-    my_ivi.tap_on_recommend_movie()
+    @allure.title("Select movie in recommends")
+    @allure.feature("Recommends section")
+    @allure.label("microservice", "My Ivi")
+    @allure.label("owner", "allure8")
+    @allure.tag("regress", "mobile")
+    @allure.severity('normal')
+    def test_select_movie_in_recommends(self):
+        # WHEN
+        my_ivi.tap_on_recommend_movie()
 
-    # THEN
-    my_ivi.button_watch_should_be_shown()
+        # THEN
+        my_ivi.button_watch_should_be_shown()
